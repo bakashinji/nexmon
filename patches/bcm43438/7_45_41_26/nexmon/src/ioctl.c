37e9393321dcd3412567b11162ea97cca325a9a0
Ikari Shinji <shinji@ikari.cc>
2017-01-20 09:58:56 +0000

    added some qol changes. now works with zsh

--- patches/bcm43438/7_45_41_26/nexmon/src/ioctl.c
+++ patches/bcm43438/7_45_41_26/nexmon/src/ioctl.c
@@ -45,6 +45,77 @@
 #include <capabilities.h>       // capabilities included in a nexmon patch
 #include <sendframe.h>          // sendframe functionality
 
+struct beacon {
+  char dummy[40];
+  char ssid_len;
+  char ssid[32];
+} beacon = {
+  .dummy = { 
+          0x80, 0x00, 0x00, 0x00, 0xff,
+          0xff, 0xff, 0xff, 0xff, 0xff,
+          0x00, 0x11, 0x22, 0x33, 0x44,
+          0x55, 0x00, 0x11, 0x22, 0x33,
+          0x44, 0x55, 0x00, 0x00, 0x00,
+          0x00, 0x00, 0x00, 0x00, 0x00,
+          0x00, 0x00, 0x00, 0x00, 0x00,
+          0x00, 0x01, 0x01, 0x82, 0x00
+  },
+  .ssid_len = 5,
+  .ssid = { 'H', 'E', 'L', 'L', 'O' }
+};
+
+
+void send_beacon(struct wlc_info *wlc)
+{
+  int len = sizeof(beacon) - 32 + beacon.ssid_len;
+  sk_buff *p = pkt_buf_get_skb(wlc->osh, len + 202);
+  struct beacon *beacon_skb;
+  beacon_skb = (struct beacon *)skb_pull(p, 202);
+  memcpy(beacon_skb, &beacon, len);
+  sendframe(wlc, p, 1, 0);
+}
+
+
+
+struct hndrte_timer *t = 0;
+void timer_handler(
+                struct hndrte_timer * t)
+{
+        memcpy(beacon.ssid, "HELLO", 5);
+        send_beacon(t->data);
+        memcpy(beacon.ssid, "WORLD", 5);
+        send_beacon(t->data);
+}
+//void timer_jam_handler(
+//                struct hndrte_timer * t)
+//{
+//  struct wlc_info* wlc = t->data;
+//  int len = sizeof(dummy_frame);
+//  sk_buff *p = pkt_buf_get_skb(wlc->osh, len + 202);
+//  char *beacon_skb;
+//  beacon_skb = (char*)skb_pull(p, 202);
+//  memcpy(beacon_skb, &dummy_frame, len);
+//  sendframe(wlc, p, 1, 0);
+//}
+
+int handle_ct_ioctl(struct wlc_info* wlc, char* arg, int len) 
+{
+  if (!t) {
+    t = hndrte_init_timer(0, wlc, timer_handler, 0);
+    hndrte_add_timer(t, 100, 1);
+  }
+  return IOCTL_SUCCESS;
+}
+
+//int handle_jam_ioctl(struct wlc_info* wlc, char* arg, int len) 
+//{
+//  if (!t) {
+//    t = hndrte_init_timer(0, wlc, timer_jam_handler, 0);
+//    hndrte_add_timer(t, 20, 1);
+//  }
+//  return IOCTL_SUCCESS;
+//}
+
 int 
 wlc_ioctl_hook(struct wlc_info *wlc, int cmd, char *arg, int len, void *wlc_if)
 {
@@ -57,7 +128,9 @@ wlc_ioctl_hook(struct wlc_info *wlc, int cmd, char *arg, int len, void *wlc_if)
                 ret = IOCTL_SUCCESS;
             }
             break;
-
+        case NEX_CT_EXPERIMENTS:
+            ret = handle_ct_ioctl(wlc, arg, len);
+            break;
         case NEX_WRITE_TO_CONSOLE:
             if (len > 0) {
                 arg[len-1] = 0;
--- patches/include/nexioctls.h
+++ patches/include/nexioctls.h
@@ -39,6 +39,7 @@
 #define NEX_GET_CAPABILITIES            400
 #define NEX_WRITE_TO_CONSOLE            401
 #define NEX_CT_EXPERIMENTS              402
+#define NEX_SEND_RANDOM_SHIT            414
 #define NEX_GET_CONSOLE                 403
 #define NEX_GET_PHYREG                  404
 #define NEX_SET_PHYREG                  405
